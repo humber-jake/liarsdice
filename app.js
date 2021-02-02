9fe553d831acfbcda55073efec61f6484279c83d
humber-jake <humber.jake@gmail.com>
2021-02-01 20:30:54 -0800

    delete users and empty rooms on disconnect

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+function isObjectEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
 let users = {};
     rooms = {};
 
@@ -15,13 +19,6 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log(`The user ${socket.id} connected`);
-  socket.on('disconnect', () => {
-    console.log('disconnect!');
-    // users = users.filter(u => u.id !== socket.id);
-  })
-});
-
-io.on('connection', (socket) => {
   socket.on('join or create room', (roomID, username) => {
     class User {
       constructor(username, roomID){
@@ -38,22 +35,35 @@ io.on('connection', (socket) => {
         }
       }
     const newUser = new User(username, roomID);
-    const newRoom = new Room(roomID);
-    newRoom.users[socket.id] = newUser;
-    rooms[roomID] = newRoom;
+    if(!rooms[newUser.currentRoom]){
+        const newRoom = new Room(roomID);
+        newRoom.users[socket.id] = newUser;
+        rooms[roomID] = newRoom;
+      } else {
+        rooms[newUser.currentRoom].users[newUser.id] = newUser;
+        socket.join(newUser.currentRoom);
+      }
     users[socket.id] = newUser;
+    console.log('==============')
+    console.log('ROOMS')
     console.log('==============')
     console.log(rooms)
     console.log('==============')
+    console.log('USERS')
+    console.log('==============')
     console.log(users)
 
-    socket.join(roomID);
     socket.emit('message', rooms, users);
+    socket.on('disconnect', () => {
+      console.log(`${socket.id} disconnected!`);
+      delete users[socket.id];
+      if(rooms[roomID].users){delete rooms[roomID].users[socket.id]}
+      if(isObjectEmpty(rooms[roomID].users) === true)
+      {delete rooms[roomID]}
+    })
   })
+
 });
-
-
- 
 
 http.listen(port, () => {
     console.log(`Server listening on port ${port}`);
